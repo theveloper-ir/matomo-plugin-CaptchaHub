493dd90e9dd707c561eeee74bd723d186130d2f1
Alireza <artaorggroup@gmail.com>
2024-08-07 15:06:45 +0330

    add main variable for CloudflareTurnstileValidator Class

--- Validation/CloudflareTurnstileValidator.php
+++ Validation/CloudflareTurnstileValidator.php
@@ -2,7 +2,15 @@
 
 namespace Piwik\Plugins\CaptchaHub\Validation;
 
+use Piwik\Plugins\CaptchaHub\SystemSettings;
+
 class CloudflareTurnstileValidator
 {
+    private $secretKey;
 
+    public function __construct()
+    {
+        $settings = new SystemSettings();
+        $this->secretKey = $settings->secretKey->getValue();
+    }
 }
