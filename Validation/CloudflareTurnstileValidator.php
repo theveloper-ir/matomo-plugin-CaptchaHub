7efa8407179e3ef446da6248323de190326137aa
Alireza <artaorggroup@gmail.com>
2024-08-07 15:08:04 +0330

    add validate method on CloudflareTurnstileValidator Class Cloudflare Captcha

--- Validation/CloudflareTurnstileValidator.php
+++ Validation/CloudflareTurnstileValidator.php
@@ -2,6 +2,7 @@
 
 namespace Piwik\Plugins\CaptchaHub\Validation;
 
+use Piwik\Piwik;
 use Piwik\Plugins\CaptchaHub\SystemSettings;
 
 class CloudflareTurnstileValidator
@@ -13,4 +14,20 @@ class CloudflareTurnstileValidator
         $settings = new SystemSettings();
         $this->secretKey = $settings->secretKey->getValue();
     }
+
+    /**
+     * Validate the captcha response token.
+     *
+     * @param string $responseToken
+     * @return bool
+     */
+    public function validate($responseToken)
+    {
+        if (empty($responseToken)) {
+            throw new \InvalidArgumentException(Piwik::translate('CaptchaHub_CaptchaNotChecked'));
+        }
+
+        return $this->isCaptchaValid($responseToken);
+    }
+    
 }
