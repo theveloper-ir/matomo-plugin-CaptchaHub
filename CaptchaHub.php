9d8fd20be09b2dad85978620b7a4992621445e21
Alireza <artaorggroup@gmail.com>
2024-08-07 15:00:30 +0330

    add addCaptchaToTemplateHelper method for handle login element template and .htaccess files

--- CaptchaHub.php
+++ CaptchaHub.php
@@ -159,4 +159,65 @@ class CaptchaHub extends \Piwik\Plugin
 
     }
 
+    private function addCaptchaToTemplateHelper($scriptSrc, $captchaClass)
+    {
+        if(!$this->settings->captchaStatus->getValue())
+        {
+            $db = Db::get();
+            $db->update(Common::prefixTable('plugin_setting'),['setting_value'=>1],'plugin_name = "CaptchaHub" and setting_name = "captchaStatus"');
+        }
+
+        $siteKey = isset($_POST['settingValues']['CaptchaHub'][2]['value'])?$_POST['settingValues']['CaptchaHub'][2]['value']:$this->settings->siteKey->getValue();
+        $fileAddress = "Login/templates/login.twig";
+        $templateContent = $this->getTemplateContent($fileAddress);
+
+        $containsStartCaptcha = strpos($templateContent, self::START_CAPTCHA) !== false;
+        $containsEndCaptcha = strpos($templateContent, self::END_CAPTCHA) !== false;
+        $containsCaptchaTag = strpos($templateContent, $captchaClass) !== false;
+
+        $captchaHtml = PHP_EOL.'
+        ' . self::START_CAPTCHA . '
+        <div class="row">
+            <div class="col s12 input-field">
+                <script src="' . $scriptSrc . '" async defer></script>
+                <div  '. $captchaClass .' data-sitekey="' . $siteKey . '" tabindex="30"></div>
+            </div>
+        </div>
+        ' . self::END_CAPTCHA . PHP_EOL;
+
+
+        if (!$containsStartCaptcha && !$containsEndCaptcha) {
+
+            $search = '<label for="login_form_password"><i class="icon-locked icon"></i> {{ \'General_Password\'|translate }}</label>
+                        </div>
+                    </div>';
+
+            $position = strpos($templateContent, $search);
+
+            if ($position !== false) 
+            {
+                
+                $position += strlen($search);
+
+                $newFileContent = substr($templateContent, 0, $position) . $captchaHtml . substr($templateContent, $position);
+
+                $this->putTemplateContent($fileAddress, $newFileContent);
+                $this->putHtaccessContent(isset($_POST['settingValues']['CaptchaHub'][1]['value'])?$_POST['settingValues']['CaptchaHub'][1]['value']:$this->settings->captchaProvider->getValue());
+
+            }
+        } 
+        else if (!$containsCaptchaTag || $siteKey != $this->settings->siteKey->getValue()) 
+        {
+
+            $startCaptchaPos = strpos($templateContent, self::START_CAPTCHA);
+            $endCaptchaPos = strpos($templateContent, self::END_CAPTCHA);
+
+            $newFileContent = substr($templateContent, 0, $startCaptchaPos) . $captchaHtml . substr($templateContent, $endCaptchaPos + strlen(self::END_CAPTCHA));
+            
+            $this->putTemplateContent($fileAddress, $newFileContent);
+            $this->putHtaccessContent($_POST['settingValues']['CaptchaHub'][1]['value']);
+
+        }
+    }
+    
 }
