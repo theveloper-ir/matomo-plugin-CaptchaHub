f7bfb369775b1622178e6a1abeb9a9e23e095fcf
Alireza <artaorggroup@gmail.com>
2024-08-07 14:34:26 +0330

    add neccessary variable on main CaptchaHub plugin file

--- CaptchaHub.php
+++ CaptchaHub.php
@@ -11,4 +11,18 @@ namespace Piwik\Plugins\CaptchaHub;
 
 class CaptchaHub extends \Piwik\Plugin
 {
+    private $settings;
+    protected $pluginName = 'CaptchaHub';
+
+    const START_CAPTCHA = '<!-- Start Captcha -->';
+    const END_CAPTCHA = '<!-- End Captcha -->';
+    const GOOGLE_RECAPTCHA_TAGS = 'class="g-recaptcha"';
+    const CLOUDFLARE_TURNSTILE_TAGS = 'class="cf-turnstile"';
+
+
+    public function __construct()
+    {
+        $this->settings = new SystemSettings();
+    }
+
 }
