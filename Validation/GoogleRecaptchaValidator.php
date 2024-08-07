10a86ed16596d5f38361603e5a1fe5e072dd06c7
Alireza <artaorggroup@gmail.com>
2024-08-07 14:42:35 +0330

    add validate method on GoogleRecaptchaValidator Class validate Google Recaptcha

--- Validation/GoogleRecaptchaValidator.php
+++ Validation/GoogleRecaptchaValidator.php
@@ -14,4 +14,19 @@ class GoogleRecaptchaValidator
         $settings = new SystemSettings();
         $this->secretKey = $settings->secretKey->getValue();
     }
+
+    /**
+     * Validate the captcha response token.
+     *
+     * @param string $responseToken
+     * @return bool
+     */
+    public function validate($responseToken)
+    {
+        if (empty($responseToken)) {
+            throw new \InvalidArgumentException(Piwik::translate('CaptchaHub_CaptchaNotChecked'));
+        }
+
+        return $this->isCaptchaValid($responseToken);
+    }
 }
