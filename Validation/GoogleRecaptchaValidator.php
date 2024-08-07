e7fbead9e72c712ac934ccecd4783d46dd2d88b0
Alireza <artaorggroup@gmail.com>
2024-08-07 14:41:37 +0330

    add main variable for GoogleRecaptchaValidator Class

--- Validation/GoogleRecaptchaValidator.php
+++ Validation/GoogleRecaptchaValidator.php
@@ -2,7 +2,16 @@
 
 namespace Piwik\Plugins\CaptchaHub\Validation;
 
+
+use Piwik\Plugins\CaptchaHub\SystemSettings;
+
 class GoogleRecaptchaValidator
 {
-   
+    private $secretKey;
+
+    public function __construct()
+    {
+        $settings = new SystemSettings();
+        $this->secretKey = $settings->secretKey->getValue();
+    }
 }
