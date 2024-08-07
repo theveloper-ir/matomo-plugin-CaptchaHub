aa08ebab189e6d56184ef0dd999a7b5a88b1f8fd
Alireza <artaorggroup@gmail.com>
2024-08-07 14:51:54 +0330

    add 2 methods for put and remove main .htaccess content

--- CaptchaHub.php
+++ CaptchaHub.php
@@ -84,5 +84,56 @@ class CaptchaHub extends \Piwik\Plugin
 
         return $bytesWritten;
     }
+    
+    private function putHtaccessContent($captchaProvider)
+    {
+        $rootPath = PIWIK_INCLUDE_PATH;
+        $path = $rootPath . DIRECTORY_SEPARATOR . ".htaccess";
+
+        if (!file_exists($path)) {
+            file_put_contents($path, '');
+        }
+
+        $htaccessContent = file_get_contents($path);
+
+        //return if captcha Provider equal htaccess Content
+        $pattern = "/#{$captchaProvider} Headers.*?#End {$captchaProvider} Headers/s";
+        if(preg_match($pattern, $htaccessContent))
+            return;
+
+        // Remove all previous captcha headers
+        $htaccessContent = $this->removeCaptchaHeaders($htaccessContent);
+
+        // Add new captcha headers based on provider
+        switch ($captchaProvider) 
+        {
+            case 'googleRecaptcha':
+                $htaccessContent = $this->addGoogleHeaders($htaccessContent);
+                break;
+        }
+
+        $bytesWritten = file_put_contents($path, $htaccessContent);
+
+        if ($bytesWritten === false) {
+            throw new \Exception('Failed to write to file: ' . $path);
+        }
+
+        return $bytesWritten;
+    }
+
+    private function removeCaptchaHeaders($content)
+    {
+        // Define patterns for different captcha headers
+        $patterns = [
+            '/#googleRecaptcha Headers.*?#End googleRecaptcha Headers/s',
+        ];
+
+        // Remove all matching patterns
+        foreach ($patterns as $pattern) {
+            $content = preg_replace($pattern, '', $content);
+        }
+
+        return $content;
+    }
 
 }
